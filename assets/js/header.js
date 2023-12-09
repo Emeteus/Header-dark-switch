6e8980ac14a967ae7b005e7d6863d51e2562e8fb
Emeteus <abodemaxmod2002@gmail.com>
2023-12-09 16:21:47 +0200

    new header

---
+++ assets/js/header.js
@@ -0,0 +1,5 @@
+document.querySelector(".hamburger").addEventListener("click", function () {
+    document.querySelector(".header").classList.toggle("mobile");
+    document.querySelector("body").classList.toggle("hidden");
+  });
+  
--- assets/js/main.js
+++ assets/js/main.js
@@ -11,16 +11,4 @@ const showMenu = (toggleId, navId) =>{
 
 showMenu('menu-toggle','nav-menu')
 
-  let prevScrollPos = window.pageYOffset;
 
-  window.onscroll = function () {
-    const currentScrollPos = window.pageYOffset;
-
-    if (prevScrollPos > currentScrollPos) {
-      document.querySelector(".l-header").style.top = "0";
-    } else {
-      document.querySelector(".l-header").style.top = `-${document.querySelector(".l-header").offsetHeight}px`;
-    }
-
-    prevScrollPos = currentScrollPos;
-  };
